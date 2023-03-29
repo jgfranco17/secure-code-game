59fc6d99aa3d42827a92d6a4ed44d996e202d75b
Chino Franco <chino.franco@gmail.com>
2023-03-30 02:00:04 +0900

    Completed attempt

--- Level-1/attempt.py
+++ Level-1/attempt.py
@@ -19,17 +19,24 @@ Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
+    MAX_ITEM_AMOUNT = 100_000
+    MAX_QUANTITY = 1000
+    MAX_TOTAL = 1_000_000
     net = 0
     
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if -MAX_ITEM_AMOUNT < item.amount < MAX_ITEM_AMOUNT:
+                net += item.amount
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            if (0 < item.quantity <= MAX_QUANTITY) and (0 < item.amount <= MAX_ITEM_AMOUNT):
+                net -= item.amount * item.quantity
+            if (-MAX_TOTAL > net) or (net > MAX_TOTAL):
+                return "Total amount exceeded"
         else:
-            return("Invalid item type: %s" % item.type)
+            return f'Invalid item type: {item.type}'
     
     if net != 0:
-        return f'Order ID: {order.id} - Payment imbalance: ${net:0.2f}' 
+        return f'Order ID: {order.id} - Payment imbalance: ${net:.2f}' 
     else:
         return f'Order ID: {order.id} - Full payment received!'
